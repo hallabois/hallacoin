9f5d21ab6819788f611d046931c4cea3859def22
Elias Eskelinen <elias.eskelinen@pm.me>
2021-05-20 15:58:08 +0300

    Update base58Prefixes to unique values

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -126,12 +126,12 @@ public:
         vSeeds.emplace_back("dnsseed.Hallacointools.com");
         vSeeds.emplace_back("dnsseed.Hallacoinpool.org");
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,40);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
         base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,50);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,176);
-        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
-        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,40);
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x48, 0x88, 0xB2, 0x1E};
+        base58Prefixes[EXT_SECRET_KEY] = {0x48, 0x88, 0xAD, 0xE4};
 
         bech32_hrp = "ltc";
 
