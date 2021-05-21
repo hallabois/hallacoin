1d2ed8d11060fb504641596d045bf65a840dbab0
Elias Eskelinen <elias.eskelinen@pm.me>
2021-05-21 10:15:59 +0300

    Update lint_06_script.sh

--- .travis/lint_06_script.sh
+++ .travis/lint_06_script.sh
@@ -4,7 +4,7 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-exit 1 #Disable for now
+# exit 1 #Disable for now
 
 export LC_ALL=C
 
