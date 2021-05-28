e565f9bd5ef68120aae0d408281400b0e3e1bf72
LaakJoonas <joonas.laakkonen04@gmail.com>
2021-05-28 14:52:36 +0300

    lmao korjattiin lyhenteet

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -54,8 +54,8 @@ QString BitcoinUnits::shortName(int unit)
 {
     switch(unit)
     {
-    case uBTC: return QString::fromUtf8("bits");
-    case SAT: return QString("sat");
+    case uBTC: return QString::fromUtf8("eximiat");
+    case SAT: return QString("improbatur");
     default: return longName(unit);
     }
 }
